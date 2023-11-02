7d932192d4b7668eaacd1c5fd912f5f70bba568d
Ioann Stanković <ioann.stankovic@gmail.com>
2023-11-02 15:50:02 +0100

    Flappy birbs
    Poizkus flappy birds s Unity.

---
+++ Flappy intro/Assets/PipeMiddleScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMiddleScript : MonoBehaviour
+{
+    public LogicScript logic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        logic.addScore(1);
+    }
+}
---
+++ Flappy intro/Assets/PipeSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float HeightOffset = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }else
+        {
+
+
+            spawnPipe();
+            timer = 0;
+        }
+    }
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - HeightOffset;
+        float highestPoint = transform.position.x + HeightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint/3,highestPoint/3),0), transform.rotation);
+    }
+}
